f2bebfa1d0763f6210ae4766a7b74539578081fc
mstaicu <md.staicu@yahoo.com>
2017-05-20 18:50:10 +0300

    :bug: Adding NODE_ENV to the production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,40 +1,46 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    HTMLWebpackPlugin = require('html-webpack-plugin'),
-    webpack = require('webpack');
+  HTMLWebpackPlugin = require('html-webpack-plugin'),
+  webpack = require('webpack');
 
 module.exports = {
-    entry: [
-        'babel-polyfill',
-        './app/index.js'
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel"
-            }, {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!postcss')
-            }
-        ]
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: "index_bundle.js"
-    },
-    postcss: [require('postcss-cssnext')()],
-    plugins: [
-        new ExtractTextPlugin('styles.css', {allChunks: true}),
-        new HTMLWebpackPlugin({
-            template: __dirname + '/app/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        }),
-        new webpack.optimize.UglifyJsPlugin()
-    ],
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
+  entry: [
+    'babel-polyfill', './app/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel"
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!postcss')
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: "index_bundle.js"
+  },
+  postcss: [require('postcss-cssnext')()],
+  plugins: [
+    new ExtractTextPlugin('styles.css', {allChunks: true}),
+    new HTMLWebpackPlugin({
+      template: __dirname + '/app/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      process: {
+        env: {
+          NODE_ENV: "'production'"
+        }
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
 }
